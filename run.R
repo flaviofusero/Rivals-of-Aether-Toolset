312be31d5346823ef41d9c43533b729876377208
Claudio Fusero <flavio.fusero@gmail.com>
2021-09-15 23:25:06 +0200

    Initial commit

--- run.R
+++ run.R
@@ -1,4 +1,18 @@
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinyjs)
+library(plotly)
+library(data.table)
+library(stringr)
+library(openxlsx)
+library(glue)
+library(Rcpp)
+library(htmlwidgets)
+library(DT)
+source('js/click_anywhere.js')
+sourceCpp('cpp/utils.cpp')
+
 port <- Sys.getenv('PORT')
 shiny::runApp(
   appDir = getwd(),
