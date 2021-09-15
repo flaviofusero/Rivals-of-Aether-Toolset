392aa5bc2ba21c52c628515ae31c51f06f764a30
Claudio Fusero <flavio.fusero@gmail.com>
2021-09-15 23:33:57 +0200

    Initial commit

---
+++ init.R
@@ -0,0 +1,20 @@
+my_packages = c(""shiny",
+"shinydashboard",
+"shinyWidgets",
+"shinyjs",
+"plotly",
+"data.table",
+"stringr",
+"openxlsx",
+"glue",
+"Rcpp",
+"htmlwidgets",
+"DT")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
